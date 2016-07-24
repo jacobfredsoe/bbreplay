ee32fed1933b15815750f04f32b3af469a4b51e1
ricoud <ricoudj@gmail.com>
2016-07-24 15:40:36 +0200

    getMatchResults first implementation

--- main.js
+++ main.js
@@ -2,11 +2,49 @@
 
 'use strict';
 
-var xml2js = require('xml2js'),
-    StreamZip = require('node-stream-zip');
+// include npm packages
+var async = require('async');
 
+// include modules libraries
+var check = require('./lib/check'),
+    bb2Helpers = require('./lib/bb2Helpers');
 
-module.exports = function(files) {
-  if (typeof files === 'undefined')
-    return null;
+/**
+ * Return Blood Bowl Match result
+ * @param {object} param - the parameters of the function
+ * @param {requestCallback} next - callback function
+ * @return {requestCallback} next
+ */
+var getMatchResults = function (param, next) {
+  async.series([
+    function (cb) {
+      check.param(param, function (err) {
+        if (err)
+          return cb(err);
+        cb(null);
+      });
+    },
+    function (cb) {
+      check.fileExist(param.replay, function (err) {
+        if (err)
+          return cb(err);
+        cb(null);
+      });
+    },
+    function (cb) {
+      bb2Helpers.readReplay(param.replay, function (err, replay) {
+        if (err)
+          return cb(err);
+        cb(null, replay);
+      });
+    }
+  ], function (err, res) {
+    if (err)
+      return next(err);
+    return next(null, res[2]);
+  });
+};
+
+module.exports = {
+  getMatchResults: getMatchResults
 };
--- test/main-test.js
+++ test/main-test.js
@@ -1,10 +1,55 @@
-var chai = require('chai');
-var expect = chai.expect;
+'use strict';
+
+var chai = require('chai'),
+    expect = chai.expect,
+    assert = chai.assert,
+    should = chai.should();
+
 var bbreplay = require('../main');
 
-describe('main', function () {
-  it('bbreplay() should return null if no file are passed in', function () {
-    var data = bbreplay();
-    expect(data).to.equal(null);
-  });
+describe('test bbreplay.matchResults', function () {
+
+  it('should return error if params is an empty object',
+      function (done) {
+        bbreplay.getMatchResults({}, function (err, res) {
+          expect(err).to.be.an('object');
+          done();
+        });
+      });
+
+  it('should return an error if param.replay is not an existing file',
+      function (done) {
+        var data = {
+          replay: 'xxxxx'
+        };
+        bbreplay.getMatchResults(data, function (err, res) {
+          assert.notEqual(err, null);
+          done();
+        });
+      });
+
+  it('should return an error if param.replay is not a valid replay file',
+      function (done) {
+        var data = {
+          replay: './test/replay.md'
+        };
+        bbreplay.getMatchResults(data, function (err, res) {
+          assert.notEqual(err, null);
+          done();
+        });
+      });
+      
+  it('should return no error and matchresult object if param is an object with valid replay file',
+      function (done) {
+        var data = {
+          replay: './test/1545597_Coach-81993-95c8420d237834b32549aca42dfe965c_2016-03-01_13_19_40.bbrz'
+        };
+        this.timeout(20000);
+        bbreplay.getMatchResults(data, function (err, res) {
+          assert.equal(err, null);
+          expect(res).to.be.an('object');
+          done();
+        });
+      });
+      
 });
